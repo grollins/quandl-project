143648067f848c1b0f1594509d37837b0a1ee758
Geoff Rollins <grollins@gmail.com>
2017-07-05 14:49:36 -0700

    Implement basic Luigi Quandl sync pipeline

--- backfill_stock_prices.py
+++ backfill_stock_prices.py
@@ -1,36 +1,55 @@
+import os
+import json
 import luigi
+import pandas as pd
 
-class SyncPrice(luigi.Task):
+
+class MakeTapConfig(luigi.Task):
     ticker = luigi.Parameter()
 
     def requires(self):
         return []
 
     def output(self):
-        return luigi.LocalTarget("{}.csv".template(ticker))
+        return luigi.LocalTarget('config/%s.json' % self.ticker)
 
     def run(self):
-        # write config
-        config_file = '{}_config.json'.template(ticker)
-        with open(config_file, 'w') as f:
-            json.dumps({'start_date': '2017-06-01', 'end_date': '2017-06-03',
-                        'ticker': ticker})
-        sys.cmd('tap-quandl-stock-price -c {} | target_csv -c csv_config.json'.template(config_file))
+        with self.output().open('w') as f:
+            json.dump({'start_date': '2017-05-01', 'end_date': '2017-06-03',
+                       'ticker': self.ticker}, f)
 
-class SquaredNumbers(luigi.Task):
+
+class SyncPrice(luigi.Task):
+    ticker = luigi.Parameter()
 
     def requires(self):
-        return [SyncPrice(ticker='FB'), SyncPrice(ticker='SBUX')]
+        return MakeTapConfig(ticker=self.ticker)
 
     def output(self):
-        return luigi.LocalTarget("stock_price.csv")
+        return luigi.LocalTarget('output/%s.csv' % self.ticker)
 
     def run(self):
-        with self.input()[0].open() as fin, self.output().open('w') as fout:
-            for line in fin:
-                n = int(line.strip())
-                out = n * n
-                fout.write("{}:{}\n".format(n, out))
+        tap_cmd = 'tap-quandl-stock-price -c %s' % self.input().fn
+        target_cmd = 'target-csv -c csv_config.json -o %s' % self.output().fn
+        os.system('%s | %s' % (tap_cmd, target_cmd))
+
+
+class QuandlSync(luigi.Task):
+
+    def requires(self):
+        task_list = []
+        with open('ticker_symbols.txt', 'r') as f:
+            task_list = [SyncPrice(ticker.strip()) for ticker in f.readlines()]
+        return task_list
+
+    def output(self):
+        return luigi.LocalTarget('output/stock_price.csv')
+
+    def run(self):
+        input_filenames = [x.fn for x in self.input()]
+        df_list = [pd.read_csv(fn) for fn in input_filenames]
+        df = pd.concat(df_list)
+        df.to_csv(self.output().fn, index = False)
 
 if __name__ == '__main__':
     luigi.run()
